761c7bbaebcb9dff260f47918ea40ab939e955b4
Nikolai Stoqnov <stoqnovn@yahoo.com>
2015-11-01 22:48:15 +0200

    Модул09 Интегриране с Excel

---
+++ Interop/Excel/IOWrite.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using InteropExcel = Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+
+namespace Excel
+{
+	public class IOWrite
+	{
+		private DataStruct _data;
+		private InteropExcel.Application excel;
+
+		public IOWrite (DataStruct data)
+		{
+			_data = data;
+		}
+
+		public bool exportTable ()
+		{
+			try
+			{
+				//Подготовка
+				excel = new InteropExcel.ApplicationClass ();
+				if ( excel == null ) return false;
+
+				excel.Visible = false;
+
+				InteropExcel.Workbook workbook = excel.Workbooks.Add ();
+				if ( workbook == null ) return false;
+
+				InteropExcel.Worksheet sheet = (InteropExcel.Worksheet) workbook.Worksheets [1];
+				sheet.Name = "Таблица1";
+
+				//Попълване на таблицата
+
+				int i = 1;
+
+				addRow ( new DataRow ( "Първо име", "Фамилия", "Години"), i++, true, 50); i++;
+
+				foreach (DataRow row in _data.table )
+				{
+					addRow (row, i++, false, -1 );
+				}
+
+				i++;  addRow ( new DataRow ( "Брой редове", "", _data.table.Count.ToString () ), i++, true, -1); 
+
+
+
+
+				//Запаметяване и затваряне
+				workbook.SaveCopyAs ( getPath () );
+
+				excel.DisplayAlerts = false;  //Изключваме всички съобщения на Excel
+
+				workbook.Close ();
+				excel.Quit ();
+
+				//Освобождаване на паметта от Excel
+				if ( workbook != null )		Marshal.ReleaseComObject (workbook);
+				if ( sheet != null )		Marshal.ReleaseComObject (sheet);
+				if ( excel != null )		Marshal.ReleaseComObject (excel);
+
+				workbook = null;
+				sheet    = null;
+				excel    = null;
+
+				GC.Collect ();
+
+				return true;
+			}catch{
+			}
+
+			return false;
+		}
+		public void addRow ( DataRow _dataRow, int _indexRow, bool isBold, int color )
+		{
+			try {
+
+				InteropExcel.Range range;
+				//Форматираме
+				range = excel.Range ["A" + _indexRow.ToString(), "C" + _indexRow.ToString() ];
+
+				if( color > 0 )	range.Interior.ColorIndex = color;
+				if( isBold )	range.Font.Bold = isBold;
+
+				//Въвеждане на данни клетка по клетка
+				range = excel.Range ["A" + _indexRow.ToString(), "A" + _indexRow.ToString() ];
+				range.Value2 = _dataRow.firstName;
+
+				range = excel.Range ["B" + _indexRow.ToString(), "B" + _indexRow.ToString() ];
+				range.Value2 = _dataRow.lastName;
+
+				range = excel.Range ["C" + _indexRow.ToString(), "C" + _indexRow.ToString() ];
+				range.Value2 = _dataRow.age;
+				
+			} catch {
+			}
+
+		}
+
+
+		public void runFile ()
+		{
+			try {
+				System.Diagnostics.Process.Start (getPath () );
+			} catch {
+			}
+
+		}
+
+		private string getPath ()
+		{
+
+			return System.IO.Path.Combine (AppDomain.CurrentDomain.BaseDirectory, "Table1.xlsx");
+		}
+	}
+}
+
--- Interop/Excel/Program.cs
+++ Interop/Excel/Program.cs
@@ -6,7 +6,18 @@ namespace Excel
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			DataStruct data = new DataStruct ();
+			IOWrite write = new IOWrite (data);
+
+
+			//Набиране на данни в основната таблица
+			data.addRow ("Николай", "Стоянов", "19");
+			data.addRow ("Георги", "Мартинов", "23");
+
+			//Проверка на таблица
+			data.printTable ();
+			write.exportTable ();
+			write.runFile ();
 		}
 	}
 }
